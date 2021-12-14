cb05edaf2c053d3df6efe56c1d183a5f4fb5f62c
shoji9x9 <40084422+shoji9x9@users.noreply.github.com>
2021-12-14 17:52:40 +0900

    Update install_libraries.py

--- src/install_libraries.py
+++ src/install_libraries.py
@@ -5,6 +5,3 @@ res = subprocess.run(['pip', 'install', 'category_encoders'], stdout=subprocess.
 sys.stdout.write(res.stdout)
 res = subprocess.run(['pip', 'install', '-U', 'ta'], stdout=subprocess.PIPE)
 sys.stdout.write(res.stdout)
-# from ta.volatility import BollingerBands, AverageTrueRange
-# from ta.momentum import RSIIndicator, StochasticOscillator
-# from ta.trend import MACD, PSARIndicator, KSTIndicator, MassIndex
